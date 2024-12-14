0155f9fc5968ba08b3f615f5976e3f2e755f9ff5
Vasily Vinogradov <vvinogradov2005@gmail.com>
2024-12-14 14:33:26 +0300

    add scripts

--- ddl/ods.sql
+++ ddl/ods.sql
@@ -1,36 +1,46 @@
 CREATE SCHEMA IF NOT EXISTS ods;
 
+DROP TABLE IF EXISTS ods.flight_statistics;
+
 CREATE TABLE ods.flight_statistics (
-    id SERIAL PRIMARY KEY,
-    year INT NOT NULL,
-    quarter INT NOT NULL,
-    month INT NOT NULL,
-    flight_date DATE NOT NULL,
-    reporting_airline VARCHAR(50) NOT NULL,
-    airline_tail_number VARCHAR(50),
-    airplane_id VARCHAR(50),
-    flight_number_reporting_airline VARCHAR(50),
-    origin_airport VARCHAR(10) NOT NULL,
-    dest_airport VARCHAR(10) NOT NULL,
-    dep_delay_minutes INT,
-    cancelled BOOLEAN,
+    year INTEGER,
+    quarter INTEGER,
+    month INTEGER,
+    fl_date TIMESTAMP,
+    op_unique_carrier VARCHAR(10),
+    tail_num VARCHAR(20),
+    op_carrier_fl_num INTEGER,
+    origin_airport_id VARCHAR(3),
+    dest_airport_id VARCHAR(3),
+    crs_dep_time INTEGER,
+    dep_time INTEGER,
+    dep_delay_new NUMERIC(6,2),
+    cancelled NUMERIC(3,2),
     cancellation_code VARCHAR(10),
-    weather_delay INT,
-    dep_time TIME,
-    crs_dep_time TIME,
-    distance INT,
+    air_time NUMERIC(6,2),
+    distance NUMERIC(8,2),
+    weather_delay NUMERIC(6,2),
     tech_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     tech_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+DROP TABLE IF EXISTS ods.weather_conditions;
+
 CREATE TABLE ods.weather_conditions (
-    id SERIAL PRIMARY KEY,
-    weather_date DATE NOT NULL,
-    airport_code VARCHAR(10) NOT NULL,
-    temperature FLOAT,
-    wind_speed FLOAT,
-    precipitation FLOAT,
-    weather_description VARCHAR(255),
+    local_time TEXT,
+    t TEXT,
+    p0 TEXT,
+    p TEXT,
+    u TEXT,
+    dd TEXT,
+    ff TEXT,
+    ff10 text,
+    ww TEXT,
+    w_w TEXT,
+    c TEXT,
+    vv TEXT,
+    td TEXT,
+    empty TEXT,
     tech_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     tech_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- dml/ods.sql
+++ dml/ods.sql
@@ -1,16 +1,42 @@
 COPY ods.flight_statistics (
-    year, quarter, month, flight_date, reporting_airline, airline_tail_number,
-    airplane_id, flight_number_reporting_airline, origin_airport, dest_airport,
-    dep_delay_minutes, cancelled, cancellation_code, weather_delay, dep_time,
-    crs_dep_time, distance
+    year,
+    quarter,
+    month,
+    fl_date,
+    op_unique_carrier,
+    tail_num,
+    op_carrier_fl_num,
+    origin_airport_id,
+    dest_airport_id,
+    crs_dep_time,
+    dep_time,
+    dep_delay_new,
+    cancelled,
+    cancellation_code,
+    air_time,
+    distance,
+    weather_delay
 )
-FROM '/csv_data/flight_statistics_test_data.csv'
+FROM '/csv_data/T_ONTIME_REPORTING.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY ods.weather_conditions (
-    weather_date, airport_code, temperature, wind_speed, precipitation, weather_description
+    local_time,
+    t,
+    p0,
+    p,
+    u,
+    dd,
+    ff,
+    ff10,
+    ww,
+    w_w,
+    c,
+    vv,
+    td,
+    empty
 )
-FROM '/csv_data/weather_conditions_test_data.csv'
-DELIMITER ','
+FROM PROGRAM 'tail -n +7 /csv_data/KSEA.csv'
+DELIMITER ';'
 CSV HEADER;
